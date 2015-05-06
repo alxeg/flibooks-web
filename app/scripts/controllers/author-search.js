af2a18041c7fd0db0b0e0759502910a2448041d3
alxeg <alx.egorov@gmail.com>
2015-05-06 20:19:54 +0300

    Add author search functionality

--- app/scripts/controllers/author-search.js
+++ app/scripts/controllers/author-search.js
@@ -1,15 +1,30 @@
 define(['app'], function(app) {
 
-    return ['$scope', '$timeout', function($scope, $timeout) {
+    return ['$scope', '$timeout', '$http', function($scope, $timeout, $http) {
+        $scope.authors = [];
+
         $scope.search = {
             author: ""
+        };
+
+        $scope.listBooks = function (id, event) {
+            console.log("Go to books of author id " + id);
         };
 
         $scope.$watch('search.author', function() {
             $timeout.cancel($scope.timeout);
             $scope.timeout = $timeout(function() {
                 if ($scope.search.author) {
-                    console.log($scope.search.author);
+                    $http.post('/api/author/search', {
+                            "author": $scope.search.author
+                        })
+                        .success(function(data, status) {
+                            $scope.status = status;
+                            $scope.authors = data;
+                        })
+                        .error(function(data, status) {
+                            $scope.authors = [];
+                        });
                 }
             }, 2000);
         });
